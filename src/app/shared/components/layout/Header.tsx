113130bc3909f9edf7c2214bd3445dbb01dddd3b
st-lucphan <luc.phan@supremetech.vn>
2021-11-16 10:18:26 +0700

    add h1 tag for logo

--- src/app/shared/components/layout/Header.tsx
+++ src/app/shared/components/layout/Header.tsx
@@ -9,9 +9,9 @@ export function Header() {
       <header className="header">
         <div className="container">
           <div className="header-content header-desktop">
-            <div className="logo">
+            <h1 className="logo">
               <Link to="/" className="logo-link"><img src="./assets/images/logo.png" alt="ST BLOG"/></Link>
-            </div>
+            </h1>
             <nav className="nav">
               <ul className="nav-list">
                 <li className="nav-item">
@@ -26,12 +26,17 @@ export function Header() {
                 <li className="nav-item">
                   <Link to="/auth/register" className="nav-link">Sign up</Link>
                 </li>
-                <li className="nav-item">
-                  <Link to="/auth/register" className="nav-link">
-                    <img src="./assets/icons/user.png" alt="avatar" className="avatar-img"/>
-                    <span>Username</span>
-                  </Link>
-                  <i className="fas fa-chevron-down"></i>
+                <li className="nav-item nav-dropdown">
+                    <Link to="" className="nav-link dropbtn">
+                      <i className="fa fa-user icon"></i>
+                      Username 
+                      <i className="fa fa-caret-down icon"></i>
+                    </Link>
+                    <div className="dropdown-content">
+                      <a href="#">Profile</a>
+                      <a href="#">Change Password</a>
+                      <a href="#">Logout</a>
+                    </div>
                 </li>
               </ul>
             </nav>          
