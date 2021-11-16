e7213004ae903a2b5b119a72ae72a6cd292a839e
st-nhitruong <nhi.truong@supremetech.vn>
2021-11-16 14:42:16 +0700

    update html for page article detail

--- src/app/pages/articles/childrens/ArticleDetail.tsx
+++ src/app/pages/articles/childrens/ArticleDetail.tsx
@@ -3,26 +3,41 @@ import { Link } from 'react-router-dom';
 
 const ArticleDetail = () => {
   return (
-    <div className="container">
-      <main className="article-detail-wrap">
-        <header className="article-detail-header">
+    <main className="article-detail-wrap">
+      <header className="article-detail-header">
+        <div className="article-detail-header-left">
           <ul className="article-detail-list-info">
             <li className="article-detail-item-author">
               <Link to="/">
                 <i className="fas fa-pen-fancy"></i>
                 <span>Alexey Trofimov </span>
               </Link></li>
-            <li className="article-detail-item-author">
+            <li className="article-detail-item-comment">
               <i className="far fa-comment"></i>
               <span>1 </span>
             </li>
+            <li className="article-detail-item-likes">
+              <i className="far fa-heart"></i>
+              <span>2 </span>
+            </li>
           </ul>
-          <h1 className="article-title">How Minimalism Helps Me Stay Calm</h1>
-          <div className="article-detail-header-separator"></div>
-        </header>
-
-      </main>
-    </div>
+        </div>
+        <div className="article-detail-header-right">
+          <Link to="/">
+            <i className="far fa-bookmark"></i>
+          </Link>
+        </div>
+        <h1 className="article-title">How Minimalism Helps Me Stay Calm</h1>
+        <div className="article-detail-header-separator"></div>
+      </header>
+      <section className="article-detail-content">
+        <div className="container">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+    </main>
   );
 };
 
