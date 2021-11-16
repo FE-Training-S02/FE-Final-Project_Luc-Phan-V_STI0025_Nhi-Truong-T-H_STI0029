44eb4a8b8fff8803e41288816e7efd225a8beeef
st-nhitruong <nhi.truong@supremetech.vn>
2021-11-16 08:23:51 +0700

    create header article detail

--- src/app/pages/articles/childrens/ArticleDetail.tsx
+++ src/app/pages/articles/childrens/ArticleDetail.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ArticleDetail = () => {
   return (
-    <div>This is article-detail page</div>
+    <div className="container">
+      <main className="article-detail-wrap">
+        <header className="article-detail-header">
+          <ul className="article-detail-list-info">
+            <li className="article-detail-item-author">
+              <Link to="/">
+                <i className="fas fa-pen-fancy"></i>
+                <span>Alexey Trofimov </span>
+              </Link></li>
+            <li className="article-detail-item-author">
+              <i className="far fa-comment"></i>
+              <span>1 </span>
+            </li>
+          </ul>
+          <h1 className="article-title">How Minimalism Helps Me Stay Calm</h1>
+          <div className="article-detail-header-separator"></div>
+        </header>
+
+      </main>
+    </div>
   );
 };
 
