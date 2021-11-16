2bad831fda5e052bde885131e1579595410b59a1
st-lucphan <luc.phan@supremetech.vn>
2021-11-16 09:18:26 +0700

    Make html for header and footer

--- src/app/shared/components/layout/Footer.tsx
+++ src/app/shared/components/layout/Footer.tsx
@@ -2,6 +2,48 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <div>This is Footer</div>
+    <footer className="section-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col col-6">
+            <h6 className="footer-title">About</h6>
+            <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae deserunt quod dignissimos officiis esse laboriosam temporibus aliquid maiores. Distinctio est veniam repellendus voluptate nemo nesciunt quos eaque ratione deleniti nam?</p>
+          </div>
+          <div className="col col-3">
+            <h6 className="footer-title">Categories</h6>
+            <ul className="footer-links">
+              <li><a href="#">Food</a></li>
+              <li><a href="#">Sport</a></li>
+              <li><a href="#">Education</a></li>
+            </ul>
+          </div>
+          <div className="col col-3">
+            <h6 className="footer-title">About Us</h6>
+            <ul className="footer-links">
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-8">
+            <p className="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
+              <a href="#"> ST Blog</a>.
+            </p>
+          </div>
+          <div className="col col-4">
+            <ul className="social-icons">
+              <li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
+              <li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
+              <li><a className="dribbble" href="#"><i className="fa fa-dribbble"></i></a></li>
+              <li><a className="linkedin" href="#"><i className="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+  </footer>
   );
 }
--- src/app/shared/components/layout/Header.tsx
+++ src/app/shared/components/layout/Header.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import JwtHelper from '@app/core/helpers/jwtHelper';
 
 export function Header() {
+  
   return (
-    <header>
-      <ul>
-        <li>
-          <Link to="">Home</Link>
-        </li>
-        <li>
-          <Link to="articles">Articles</Link>
-        </li>
-        <li>
-          <Link to="auth/login">Login</Link>
-        </li>
-        <li>
-          <Link to="auth/register">Register</Link>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header-content header-desktop">
+            <div className="logo">
+              <Link to="/" className="logo-link"><img src="./assets/images/logo.png" alt="ST BLOG"/></Link>
+            </div>
+            <nav className="nav">
+              <ul className="nav-list">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/articles" className="nav-link">Articles</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/auth//login" className="nav-link">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/auth/register" className="nav-link">Sign up</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/auth/register" className="nav-link">
+                    <img src="./assets/icons/user.png" alt="avatar" className="avatar-img"/>
+                    <span>Username</span>
+                  </Link>
+                  <i className="fas fa-chevron-down"></i>
+                </li>
+              </ul>
+            </nav>          
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
