1e00186fc45513d34217ccb16c4ce1f59791a484
st-nhitruong <nhi.truong@supremetech.vn>
2021-11-18 16:54:27 +0700

    fix

--- src/app/pages/articles/childrens/CreateArticle.tsx
+++ src/app/pages/articles/childrens/CreateArticle.tsx
@@ -1,5 +1,6 @@
-import Input from '@app/shared/components/partials/Input';
 import React from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const CreateArticle = () => {
   return (
@@ -40,7 +41,7 @@ const CreateArticle = () => {
             editor={ClassicEditor}
             data="<p>Hello from CKEditor 5!</p>"
             onReady={editor => {
-              // You can store the "editor" and use when it is needed.
+
               console.log('Editor is ready to use!', editor);
             }}
             onChange={(event, editor) => {
