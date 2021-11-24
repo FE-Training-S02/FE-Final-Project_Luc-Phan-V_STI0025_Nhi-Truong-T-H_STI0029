d1a4504e2caf6e2a14a044a8876d36713630d1f5
st-lucphan <luc.phan@supremetech.vn>
2021-11-24 18:09:30 +0700

    Create UI and Binding API respone data for profile page

--- src/app/pages/articles/Articles.tsx
+++ src/app/pages/articles/Articles.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '@app/shared/components/layout/Sidebar';
 
 const Articles = () => {
   return (
-    <Outlet />
+    <>
+      <Outlet />
+    </>
   );
 };
 
