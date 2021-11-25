75e3a30b97b88a020c59ed27c264c9dd16e7f8cb
st-nhitruong <nhi.truong@supremetech.vn>
2021-11-25 10:52:56 +0700

    add min-height for ck content

--- src/app/pages/articles/childrens/CreateArticle.tsx
+++ src/app/pages/articles/childrens/CreateArticle.tsx
@@ -80,85 +80,82 @@ const CreateArticle = () => {
   }, [id]);
   return (
     <>
-      <div>
-
-        <h2 className="col-8 page-title">{id ? 'Edit Article' : 'New Article'}</h2>
-        <div className="form-wrapper">
-          <form onSubmit={handleSubmit(onsubmit)}>
-            <div className="row">
-              <label className="col-2 col-form-label">Title</label>
-              <div className="col-6">
+      <h2 className="col-8 page-title">{id ? 'Edit Article' : 'New Article'}</h2>
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit(onsubmit)}>
+          <div className="row">
+            <label className="col-2 col-form-label">Title</label>
+            <div className="col-6">
+              <Input
+                type="text"
+                register={register('title', titleValidator())}
+                errors={errors.title} />
+            </div>
+          </div>
+          <div className="row">
+            <label className="col-2 col-form-label">Description</label>
+            <div className="col-6">
+              <Input
+                type="text"
+                register={register('description', descriptionValidator())}
+                errors={errors.description} />
+            </div>
+          </div>
+          <div className="row">
+            <label className="col-2 col-form-label">Tags</label>
+            <div className="col-6">
+              <Input
+                type="text" register={register('tags', requireValidator())} errors={errors.tags} />
+            </div>
+          </div>
+          <div className="row">
+            <label className="col-2 col-form-label">Status</label>
+            <div className="col-6">
+              <Select listOptions={statusOptions} defaultValue={statusOptions[0]} register={register('status', requireValidator())} />
+            </div>
+          </div>
+          <div className="row">
+            <label className="col-2 col-form-label" >Upload image</label>
+            <div className="col-6">
+              {id ?
                 <Input
-                  type="text"
-                  register={register('title', titleValidator())}
-                  errors={errors.title} />
-              </div>
-            </div>
-            <div className="row">
-              <label className="col-2 col-form-label">Description</label>
-              <div className="col-6">
+                  type="file"
+                  register={register('cover')}
+                  errors={errors.cover}
+                  onChange={() => handleChange}
+                  className="col-12"
+                /> :
                 <Input
-                  type="text"
-                  register={register('description', descriptionValidator())}
-                  errors={errors.description} />
-              </div>
-            </div>
-            <div className="row">
-              <label className="col-2 col-form-label">Tags</label>
-              <div className="col-6">
-                <Input
-                  type="text" register={register('tags', requireValidator())} errors={errors.tags} />
-              </div>
-            </div>
-            <div className="row">
-              <label className="col-2 col-form-label">Status</label>
-              <div className="col-6">
-                <Select listOptions={statusOptions} defaultValue={statusOptions[0]} register={register('status', requireValidator())} />
-              </div>
-            </div>
-            <div className="row">
-              <label className="col-2 col-form-label" >Upload image</label>
-              <div className="col-6">
-                {id ?
-                  <Input
-                    type="file"
-                    register={register('cover')}
-                    errors={errors.cover}
-                    onChange={() => handleChange}
-                    className="col-12"
-                  /> :
-                  <Input
-                    type="file"
-                    register={register('cover', requireValidator())}
-                    errors={errors.cover}
-                    onChange={() => handleChange}
-                    className="col-12"
-                  />
-                }
-                {urlImage ? <img src={urlImage} alt="cover" className="col-4" /> : ''}
-              </div>
-            </div>
-            <div className="row row-ck">
-              <label className="col-2 form-label-content">Content</label>
-              <div className="wrap-ck">
-                <CKEditor
-                  editor={ClassicEditor}
-                  data={content}
-                  onChange={(event, editor) => {
-                    const data = editor.getData();
-                    setContent(data);
-                  }
-                  }
+                  type="file"
+                  register={register('cover', requireValidator())}
+                  errors={errors.cover}
+                  onChange={() => handleChange}
+                  className="col-12"
                 />
-              </div>
+              }
+              {urlImage ? <img src={urlImage} alt="cover" className="col-4" /> : ''}
             </div>
-            <div className="row row-btn">
-              <div className="col-3">
-                <Button className="btn btn-primary btn-block" type='submit'>{id ? 'Save' : 'Submit'}</Button>
-              </div>
+          </div>
+          <div className="row row-ck">
+            <label className="col-2 col-form-label">Content</label>
+            <div className="col-6">
+              <CKEditor
+                editor={ClassicEditor}
+                data={content}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  setContent(data);
+                }
+                }
+              />
             </div>
-          </form>
-        </div>
+          </div>
+          <div className="row row-btn">
+            <div className="col-3">
+              <Button className="btn btn-primary btn-block" type='submit'>{id ? 'Save' : 'Submit'}</Button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
