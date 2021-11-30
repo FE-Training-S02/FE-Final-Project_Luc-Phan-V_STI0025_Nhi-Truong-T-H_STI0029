6707a4741d8225f3ccc6752adbbcfb5cc2835ade
st-lucphan <luc.phan@supremetech.vn>
2021-11-30 14:16:34 +0700

    display datetime of comment

--- src/app/pages/articles/partials/CommentItem.tsx
+++ src/app/pages/articles/partials/CommentItem.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { convertDate } from '@app/shared/pipes/convert-date';
 
 export function CommentItem(props) {
-  const { user, comment } = props.comment;
+  const { user, comment, createdAt } = props.comment;
   return (
     <li className="comment-item mt-2">
       <img src={user.picture || "./assets/icons/user.png"} className="avatar-comment" alt="avatar"/>
       <div className="comment-body">
         <h4 className="comment-author">{user.displayName||user.lastName}</h4>
+        <p className="created-at">{convertDate(createdAt)}</p>
         <p className="comment-content">{comment}</p>
       </div>
     </li>
