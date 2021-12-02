4ffb90474ae1b9a96d20dbf17ccbc7705f85065b
st-lucphan <luc.phan@supremetech.vn>
2021-12-02 12:13:43 +0700

    update style for CommentItem

--- src/app/pages/articles/partials/CommentItem.tsx
+++ src/app/pages/articles/partials/CommentItem.tsx
@@ -6,9 +6,11 @@ export function CommentItem(props) {
   return (
     <li className="comment-item mt-2">
       <img src={user.picture || "./assets/icons/user.png"} className="avatar-comment" alt="avatar"/>
-      <div className="comment-body">
-        <h4 className="comment-author">{user.displayName||user.lastName}</h4>
-        <p className="txt-date-time">{convertDate(createdAt)}</p>
+      <div className="comment-body ml-2">
+        <div className="comment-author">
+          <h6 className="user-name">{user.displayName || `${user.firstName} ${user.lastName}`}</h6>
+          <p className="txt-date-time">{convertDate(createdAt)}</p>
+        </div>
         <p className="comment-content">{comment}</p>
       </div>
     </li>
